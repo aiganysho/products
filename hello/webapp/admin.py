e359313bdae81b4e023f1f5ce425b937c224f463
Aiganysh <aiganyshx@gmail.com>
2021-04-05 13:24:35 +0600

    Зарегистрирован в панели админ

--- hello/webapp/admin.py
+++ hello/webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from webapp.models import Product
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'category', 'description', 'remainder', 'price']
+    list_filter = ['name']
+    search_fields = ['name', 'price']
+    fields = ['id', 'name', 'category', 'description', 'remainder', 'price']
+    readonly_fields = ['id']
+
+
+admin.site.register(Product, ProductAdmin)

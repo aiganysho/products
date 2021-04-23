2df493598b2ada4902a703b6a65fe68dddda932d
Aiganysh <aiganyshx@gmail.com>
2021-04-23 18:42:02 +0600

    created accounts

---
+++ hello/accounts/session_backend.py
@@ -0,0 +1,11 @@
+from django.contrib.sessions.backends.db import SessionStore as DbSessionStore
+
+class SessionStore(DbSessionStore):
+    def cycle_key(self):
+        data = self._session
+        key = self.session_key
+        self.create()
+        self._session_cache = data
+        if key:
+            ...  # здесь пишем логику
+            self.delete(key)
